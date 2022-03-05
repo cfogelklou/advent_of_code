93ec0cec67e6b4c75263641a596be786cec8582c
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-03-05 10:02:59 +0100

    Refactor the hardcoded input and main program. Move to separate files.

---
+++ 2021/day10/main.cpp
@@ -0,0 +1,12 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+// ////////////////////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char** argv) {
+  // The following line must be executed to initialize
+  // Google Mock (and Google Test) before running the tests.
+  ::testing::InitGoogleMock(&argc, argv);
+  return RUN_ALL_TESTS();
+}
