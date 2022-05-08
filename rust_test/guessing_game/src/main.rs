5b79f91458cf653f86961343e0d8bf12ffc6c65e
Chris Fogelklou <chris.fogelklou@volvocars.com>
2022-05-08 07:50:00 +0200

    Now the game works.

--- rust_test/guessing_game/src/main.rs
+++ rust_test/guessing_game/src/main.rs
@@ -1,15 +1,40 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
-fn main(){
+fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    println!("The secret number is: {}", secret);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        /*
+        We switch from an expect call to a match expression to move from crashing on an error to handling the error. Remember that parse returns a Result type and Result is an enum that has the variants Ok and Err. We’re using a match expression here, as we did with the Ordering result of the cmp method.
+        */
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
