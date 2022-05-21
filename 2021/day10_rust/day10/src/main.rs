96ac561a9d2293072d0ed485e996c07b0f588d64
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-05-21 15:52:12 +0200

    yep

--- 2021/day10_rust/day10/src/main.rs
+++ 2021/day10_rust/day10/src/main.rs
@@ -6,10 +6,32 @@ fn is_open_bracket(c:char)->bool {
     return c == '[' || c == '{' || c == '<' || c == '(';
 }
 
-fn check_line(s:String){
-    //println!("is: {}", s);
+fn matching_open_bracket(c:char)->char {
+    return match c {
+        ')' => '(',
+        '}' => '{',
+        ']' => '[',
+        '>' => '<',
+        _ => ' ',
+    };
+}
+
+fn matching_close_bracket(c:char)->char {
+    return match c {
+        '(' => ')',
+        '{' => '}',
+        '[' => ']',
+        '<' => '>',
+        _ => ' ',
+    };
+}
+
+fn check_line(s:String)->i64{
+    //println!("{}", s);
     let mut stack:VecDeque<char> = VecDeque::new();
-    for i in 0..s.len(){
+    let mut score = 0;
+    let mut i = 0;
+    while i < s.len() && score == 0 {
         let c:char = s.chars().nth(i).unwrap();
         if is_open_bracket(c){
             stack.push_back(c);
@@ -19,19 +41,49 @@ fn check_line(s:String){
                 println!("Stack is empty, cannot pop")
             }
             else {
-                let oe:Option<&char> = stack.back();                
-                if oe != None {
-                    let pe:&char = oe.unwrap();
-                    let e:char = *pe;
-                    if e == '('{
-                        println!("Brace brace")
-                    }
-                    
+                let oe:Option<&char> = stack.back();
+                let isok:bool = match c {
+                    ')' => oe == Some(&'('),
+                    '}' => oe == Some(&'{'),
+                    ']' => oe == Some(&'['),
+                    '>' => oe == Some(&'<'),
+                    _ => false
+                };
+                
+                if !isok{
+                    println!("{} - Expected {} but found {} instead.", s, matching_close_bracket(*oe.unwrap()), c);
                 }
-                stack.pop_back();
+
+                //if oe != None {
+                //    let pe:&char = oe.unwrap();
+                //    let e:char = *pe;
+                //    if e == '('{
+                //        println!("Brace brace")
+                //    }                    
+                //}
+                if isok {
+                    stack.pop_back();
+                }
+                else {
+                    score = match oe {
+                        Some(&'(') => 3,
+                        Some(&'[') => 57,
+                        Some(&'{') => 1197,
+                        Some(&'<') => 25137,
+                        _ => score
+                    };
+                    println!("score = {}", score);
+                }
             }
         }
+        i = i+1;
+    }
+    if score != 0 {
+        return score;
+    }
+    else {
 
+        return if stack.is_empty() { 0 } else { -1 };
     }
 }
 
@@ -51,7 +103,7 @@ fn main() {
     ];
     //vec.for_each(s:String)
     for i in 0..v.len(){
-        check_line(v[i].to_string())
+        check_line(v[i].to_string());
     }
 }
 
