2d718ab8a961a38e63e3342cb9140b4659b3ebb6
Chris Fogelklou <chris.fogelklou@polestar.com>
2022-12-01 20:22:13 +0100

    Challenge one completed, now need a list.

--- 2022/rust/day1/src/main.rs
+++ 2022/rust/day1/src/main.rs
@@ -1,6 +1,5 @@
 
 fn snacks(v:Vec<String>)->(i64, i64){    
-    let mut elves_calorie_count:Vec<i64> = Vec::new();
     let mut current_elf_idx = 0;
     let mut current_elf_sum:i64 = 0;
     let mut richest_elf_idx = -1;
@@ -8,27 +7,26 @@ fn snacks(v:Vec<String>)->(i64, i64){
 
     for i in 0..v.len() {
         let next_line = v[i].to_string();
-        let snack_calories:i64 = if next_line.len() > 0 { next_line.parse::<i64>().unwrap() } else { 0 };
+        let this_snack:i64 = if next_line.len() > 0 { next_line.parse::<i64>().unwrap() } else { 0 };
         
         // Give this elf his calories
-        current_elf_sum += snack_calories;
+        current_elf_sum += this_snack;
 
         // If this snack had no calories, then push the current snack onto this elf
-        if snack_calories == 0 {
+        if this_snack == 0 {
             // New elf
             if current_elf_sum != 0 {
-                elves_calorie_count.push(current_elf_sum);
-                current_elf_idx += 1;
                 if current_elf_sum > richest_elf_sum {
                     richest_elf_sum = current_elf_sum;
                     richest_elf_idx = current_elf_idx;
                 }
+                current_elf_idx += 1;
             }
             current_elf_sum = 0;
         }
     }
     
-    return (richest_elf_idx, richest_elf_sum);
+    return (richest_elf_idx + 1, richest_elf_sum);
 }
 
 
@@ -49,14 +47,15 @@ fn main() {
             let l:String = line.unwrap();
             v.push(l);
         }    
-        let (s,incomplete_score) = snacks(v.clone());
-        println!("main::Total score is {}", s);
-        println!("main::Incomplete score is {}", incomplete_score);
+        let (s, i) = snacks(v.clone());
+        println!("The elf with the most calories is {}", s);
+        println!("The elf's total calories are {}", i);
     }
 }
 
 
 // The test case given the samples from AoC.
+#[allow(dead_code)]
 fn test_example_1() {
     let v:Vec<String> = vec![
         "1000".to_string(),
@@ -77,9 +76,9 @@ fn test_example_1() {
     ];
 
     let (s, i) = snacks(v.clone());
-    println!("test_example_1::Total score is {}", s);
-    println!("test_example_1::Incomplete score is {}", i);
-    assert_eq!(s, 3);
+    println!("The elf with the most calories is {}", s);
+    println!("The elf's total calories are {}", i);
+    assert_eq!(s, 4);
     assert_eq!(i, 24000);
 }
 
