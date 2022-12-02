c7f6411808b3ea2eac8866590da73477d32c027a
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-12-02 06:23:52 +0100

    Used some rust learnings to make this implementation better. (#6)
    * Used some learnings to improve the implementation.

--- 2022/rust/day1/src/main.rs
+++ 2022/rust/day1/src/main.rs
@@ -1,33 +1,43 @@
 
+use std::io::{self, BufReader};
+
 fn snacks(v:Vec<String>)->(i64, i64){   
     let mut elves:Vec<i64> = Vec::new(); 
     let mut current_elf_sum:i64 = 0;
 
-    for i in 0..v.len() {
-        let next_line = v[i].to_string();
+    // A lesson in mutating variables.
+    fn create_new_elf_if_nonzero_sum(sum:&mut i64, elves:&mut Vec<i64>){
+        if *sum != 0 {
+            elves.push(*sum);
+        }
+        *sum = 0;
+    }
+
+    for next_line in v.iter() {
+
         // Parse, and handle error elegantly
         match next_line.trim().parse::<i64>(){
+            
             Ok(this_snack) => {
                 // Give this elf his calories
                 current_elf_sum += this_snack;
             },
             Err(_) => {
-                // New elf
-                if current_elf_sum != 0 {
-                    elves.push(current_elf_sum);
-                }
-                current_elf_sum = 0;
+                create_new_elf_if_nonzero_sum(&mut current_elf_sum, &mut elves);
             }
-        }
-        
+        }        
     }
+
+    // After last line, check if there is still an elf processing.
+    create_new_elf_if_nonzero_sum(&mut current_elf_sum, &mut elves);
+
     elves.sort();
     elves.reverse();
     let top_three_sum:i64 = elves[0..3].iter().sum();    
     return (top_three_sum, elves[0]);
 }
 
-fn main() {
+fn main()  -> io::Result<()> {
     use std::io::BufRead;
 
     let filename = std::env::args().nth(1).expect("Expected filename");
@@ -45,7 +55,7 @@ fn main() {
     let (s, i) = snacks(v.clone());
     println!("The top three elves carry {}", s);
     println!("The elf's total calories are {}", i);
-    
+    Ok(())
 }
 
 // The test case given the samples from AoC.
@@ -77,6 +87,40 @@ fn test_example_1() {
 }
 
 
+#[allow(dead_code)]
+fn test_example_2() {
+    use std::io::BufRead;
+    let raw_string = "
+    1000\n\
+    2000\n\
+    3000\n\
+    \n\
+    4000\n\
+    \n\
+    5000\n\
+    6000\n\
+    \n\
+    7000\n\
+    8000\n\
+    9000\n\
+    \n\
+    10000";
+
+    let b = BufReader::new(raw_string.as_bytes());
+    let mut v:Vec<String> = Vec::new();
+    for (_, line) in b.lines().enumerate() {    
+        let l:String = line.unwrap().trim().to_string();
+        v.push(l);
+    }  
+    
+    let (s, i) = snacks(v.clone());
+    println!("The top three elves have {}", s);
+    println!("The elf's total calories are {}", i);
+    assert_eq!(s, 45000);
+    assert_eq!(i, 24000);
+}
+
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -85,6 +129,11 @@ mod tests {
     #[test]
     fn t1() {
         test_example_1();
+    }
+
+    #[test]
+    fn t2() {
+        test_example_2();
     }
 
 }
