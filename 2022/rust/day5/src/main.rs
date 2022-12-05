5c6f86a5a233fabf977172129b951b09544e8b22
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-12-05 06:18:10 +0100

    Can tell the difference.

--- 2022/rust/day5/src/main.rs
+++ 2022/rust/day5/src/main.rs
@@ -18,16 +18,19 @@ fn split_into_tuples(s:String) -> ((i32, i32), (i32, i32)){
     return (range_tuples[0],range_tuples[1]);
 }
 
+#[allow(dead_code)]
 fn is_range_contained(a:(i32,i32), b:(i32,i32)) -> bool {
     return  b.0 >= a.0 && b.1 <= a.1;
 }
 
+#[allow(dead_code)]
 fn complete_containment(a:(i32,i32), b:(i32,i32)) -> bool {
     return is_range_contained(a, b) || is_range_contained(b, a);
 }
 
 
 // For part 1, split the rucksacks into two groups and get the score.
+#[allow(dead_code)]
 fn how_many_assignments_is_one_contained_in_the_other(v:Vec<String>)->i32{   
     
     let mut total: i32 = 0;
@@ -41,6 +44,7 @@ fn how_many_assignments_is_one_contained_in_the_other(v:Vec<String>)->i32{
     return total;
 }
 
+#[allow(dead_code)]
 fn partial_containment(a:(i32,i32), b:(i32,i32)) -> bool {
     // I'm too tired to think, so using a method that is guaranteed to work rather than a potentially simpler comparison
     let mut overlap = false;
@@ -56,6 +60,7 @@ fn partial_containment(a:(i32,i32), b:(i32,i32)) -> bool {
 }
 
 // For part 2, Check if there is any overlap
+#[allow(dead_code)]
 fn how_many_assignments_is_there_any_overlap(v:Vec<String>)->i32{   
     
     let mut total: i32 = 0;
@@ -69,6 +74,25 @@ fn how_many_assignments_is_there_any_overlap(v:Vec<String>)->i32{
     return total;
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum TypeOfLine {
+    Crate = 0,
+    Nothing = 1,
+    Instruction = 2
+}
+
+#[allow(dead_code)]
+fn get_type_of_line(s_in:String) -> TypeOfLine {
+    let mut t = TypeOfLine::Nothing;
+    let s = s_in.trim();
+    if (s.contains("[")) { 
+        t = TypeOfLine::Crate;
+    } else if (s.contains("move")) { 
+        t = TypeOfLine::Instruction;
+    }
+    return t;
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -77,21 +101,31 @@ mod tests {
     #[test]
     fn camp_cleanup_1() {
         
-        let raw_string = "2-4,6-8
-        2-3,4-5
-        5-7,7-9
-        2-8,3-7
-        6-6,4-6
-        2-6,4-8";
+        let raw_string = 
+       "    [D]    
+        [N] [C]    
+        [Z] [M] [P]
+         1   2   3 
+        
+        move 1 from 2 to 1
+        move 3 from 1 to 3
+        move 2 from 2 to 1
+        move 1 from 1 to 2";
     
         let v: Vec<String> = utils::test_input_to_vec(raw_string.to_string());
+        assert_eq!(TypeOfLine::Crate, get_type_of_line(v[0].to_string()));
+        assert_eq!(TypeOfLine::Crate, get_type_of_line(v[1].to_string()));
+        assert_eq!(TypeOfLine::Crate, get_type_of_line(v[2].to_string()));
+        assert_eq!(TypeOfLine::Nothing, get_type_of_line(v[3].to_string()));
+        assert_eq!(TypeOfLine::Nothing, get_type_of_line(v[4].to_string()));        
+        assert_eq!(TypeOfLine::Instruction, get_type_of_line(v[5].to_string()));
+        assert_eq!(TypeOfLine::Instruction, get_type_of_line(v[6].to_string()));
+        assert_eq!(TypeOfLine::Instruction, get_type_of_line(v[7].to_string()));
+        assert_eq!(TypeOfLine::Instruction, get_type_of_line(v[8].to_string()));
         
-        let s = how_many_assignments_is_one_contained_in_the_other(v.clone());
-        //let (i, _) = rucksack_filter_groups(v.clone());
-        println!("Overlapping sections {}", s);
-        //println!("Badge scores is {}", i);
-        assert_eq!(s, 2);
-        //assert_eq!(i, 70);
+        //let s = how_many_assignments_is_one_contained_in_the_other(v.clone());
+        //println!("Overlapping sections {}", s);
+        //assert_eq!(s, 2);
     }
 
     #[test]
