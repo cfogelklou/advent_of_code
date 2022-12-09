80791423f9585349d76a231774fbe5ae143892aa
Chris Fogelklou <chris.fogelklou@polestar.com>
2022-12-09 20:33:25 +0100

    wip working star one

--- 2022/rust/day9/src/main.rs
+++ 2022/rust/day9/src/main.rs
@@ -1,4 +1,8 @@
-use std::{ cmp, collections::{ HashMap, VecDeque }, io::{ self } };
+use std::{
+    cmp,
+    collections::{HashMap, VecDeque},
+    io::{self},
+};
 mod utils;
 
 #[allow(dead_code)]
@@ -40,7 +44,7 @@ fn get_head_movement(v: &Vec<String>) -> Vec<(i32, i32)> {
 }
 
 #[allow(dead_code)]
-fn get_tail_movement(hm: &Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+fn get_tail_movement(hm: &Vec<(i32, i32)>, visualize: bool) -> Vec<(i32, i32)> {
     let mut tail_movement: Vec<(i32, i32)> = Vec::new();
     let mut x = 0;
     let mut y = 0;
@@ -48,8 +52,10 @@ fn get_tail_movement(hm: &Vec<(i32, i32)>) -> Vec<(i32, i32)> {
     for head in hm {
         let (hx, hy) = head.clone();
 
-        println!("before moving tail");
-        draw_it((5, 5), head.clone(), (x, y));
+        if visualize {
+            println!("before moving tail");
+            draw_it((5, 5), head.clone(), (x, y));
+        }
 
         let dx = hx - x;
         let dy = hy - y;
@@ -67,8 +73,10 @@ fn get_tail_movement(hm: &Vec<(i32, i32)>) -> Vec<(i32, i32)> {
             y = hy;
         }
 
-        println!("after moving tail");
-        draw_it((5, 5), head.clone(), (x, y));
+        if visualize {
+            println!("after moving tail");
+            draw_it((5, 5), head.clone(), (x, y));
+        }
 
         tail_movement.push((x, y));
     }
@@ -129,24 +137,59 @@ mod tests {
     use super::*;
 
     #[test]
-    fn scenic_test() {
-        let raw_string: String =
-            "R 4
+    fn short_tail_test() {
+        let raw_string: String = "R 4
             U 4
             L 3
             D 1
             R 4
             D 1
             L 5
-            R 2".to_string();
+            R 2"
+        .to_string();
         let v: Vec<String> = utils::test_input_to_vec(raw_string, true);
         assert_ne!(0, v.len());
         let hm = get_head_movement(&v);
-        let tm = get_tail_movement(&hm);
+        let tm = get_tail_movement(&hm, true);
         let mut unique_tm = tm.clone();
         unique_tm.sort();
         unique_tm.dedup();
         assert_eq!(13, unique_tm.len());
+    }
+
+    #[test]
+    fn long_tail_test() {
+        let raw_string: String = "R 4
+            U 4
+            L 3
+            D 1
+            R 4
+            D 1
+            L 5
+            R 2"
+        .to_string();
+        let v: Vec<String> = utils::test_input_to_vec(raw_string, true);
+        assert_ne!(0, v.len());
+        let hm = get_head_movement(&v);
+        // Part 2
+        {
+            let mut tails: Vec<Vec<(i32, i32)>> = Vec::new();
+            let num_knots = 2;
+            for i in 0..(num_knots - 1) {
+                let my_head = if i == 0 {
+                    hm.clone()
+                } else {
+                    tails[i - 1].clone()
+                };
+                let tm = get_tail_movement(&my_head, true);
+                tails.push(tm);
+            }
+            let mut unique_tm = tails[tails.len() - 1].clone();
+            unique_tm.sort();
+            unique_tm.dedup();
+            assert_eq!(13, unique_tm.len());
+            println!("Unique tail positions for part 2: {}", unique_tm.len());
+        }
     }
 }
 
@@ -159,7 +202,7 @@ fn main() -> io::Result<()> {
         String::from("input.txt")
     };
     let file = std::io::BufReader::new(
-        std::fs::File::open(<String as AsRef<std::path::Path>>::as_ref(&filename)).unwrap()
+        std::fs::File::open(<String as AsRef<std::path::Path>>::as_ref(&filename)).unwrap(),
     );
     let mut v: Vec<String> = Vec::new();
     for (_, line) in file.lines().enumerate() {
@@ -169,11 +212,37 @@ fn main() -> io::Result<()> {
     assert_ne!(0, v.len());
 
     let hm = get_head_movement(&v);
-    let tm = get_tail_movement(&hm);
-    let mut unique_tm = tm.clone();
-    unique_tm.sort();
-    unique_tm.dedup();
-    //assert_eq!(13, unique_tm.len());
-    println!("Unique tail positions: {}", unique_tm.len());
+    assert_ne!(0, v.len());
+
+    // Part 1
+    if true {
+        let tm = get_tail_movement(&hm, false);
+        let mut unique_tm = tm.clone();
+        unique_tm.sort();
+        unique_tm.dedup();
+        //assert_eq!(13, unique_tm.len());
+        println!("Unique tail positions: {}", unique_tm.len());
+    }
+
+    // Part 2
+    {
+        let mut tails: Vec<Vec<(i32, i32)>> = Vec::new();
+        let num_knots = 10;
+        for i in 0..(num_knots - 1) {
+            let my_head = if i == 0 {
+                hm.clone()
+            } else {
+                tails[i - 1].clone()
+            };
+            let tm = get_tail_movement(&my_head, false);
+            tails.push(tm);
+        }
+        let mut unique_tm = tails[tails.len() - 1].clone();
+        unique_tm.sort();
+        unique_tm.dedup();
+        //assert_eq!(13, unique_tm.len());
+        println!("Unique tail positions for part 2: {}", unique_tm.len());
+    }
+
     Ok(())
 }
