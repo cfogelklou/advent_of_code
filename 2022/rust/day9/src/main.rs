1b590d67d5e528ac264eb8dc0462e93696228aa9
Chris Fogelklou <chris.fogelklou@polestar.com>
2022-12-09 22:11:40 +0100

    Finally right answer 2562

--- 2022/rust/day9/src/main.rs
+++ 2022/rust/day9/src/main.rs
@@ -43,12 +43,13 @@ fn get_head_movement(v: &Vec<String>) -> Vec<(i32, i32)> {
     return head_movement;
 }
 
+
 #[allow(dead_code)]
 fn get_tail_movement(hm: &Vec<(i32, i32)>, visualize: bool) -> Vec<(i32, i32)> {
     let mut tail_movement: Vec<(i32, i32)> = Vec::new();
     let mut x = 0;
     let mut y = 0;
-    tail_movement.push((x, y));
+    //tail_movement.push((x, y));
     for head in hm {
         let (hx, hy) = head.clone();
 
@@ -57,49 +58,25 @@ fn get_tail_movement(hm: &Vec<(i32, i32)>, visualize: bool) -> Vec<(i32, i32)> {
             draw_it((5, 5), head.clone(), (x, y));
         }
 
-        let dx = hx - x;
-        let dy = hy - y;
-        if dy >= 2 {
-            y = hy - 1;
-            x = hx;
-        } else if dy <= -2 {
-            y = hy + 1;
-            x = hx;
-        } else if dx >= 2 {
-            x = hx - 1;
-            y = hy;
-        } else if dx <= -2 {
-            x = hx + 1;
-            y = hy;
-        }
+        let delta_x = hx - x;
+        let delta_y =  hy - y;
 
-        if visualize {
-            println!("after moving tail");
-            draw_it((5, 5), head.clone(), (x, y));
+        if (delta_x).abs() < 2 && (delta_y).abs() < 2 {
+            // Nothing to do
         }
-
+        else {
+            x += (delta_x).signum();
+            y += (delta_y).signum();
+ 
+        }
         tail_movement.push((x, y));
+
     }
     return tail_movement;
 }
 
-#[allow(dead_code)]
-fn get_play_area(v: &Vec<String>) -> (i32, i32) {
-    let mut min_x = 0;
-    let mut max_x = 0;
-    let mut min_y = 0;
-    let mut max_y = 0;
-    let hm = get_head_movement(v);
-    for (x, y) in hm {
-        min_x = cmp::min(min_x, x);
-        max_x = cmp::max(max_x, x);
-        min_y = cmp::min(min_y, y);
-        max_y = cmp::max(max_y, y);
-    }
-    let w = max_x - min_x + 1;
-    let h = max_y - min_y + 1;
-    return (w, h);
-}
+
+
 #[allow(dead_code)]
 fn draw_it(wh: (i32, i32), xy: (i32, i32), txy: (i32, i32)) {
     let (w, h) = wh;
@@ -159,22 +136,22 @@ mod tests {
 
     #[test]
     fn long_tail_test() {
-        let raw_string: String = "R 4
-            U 4
-            L 3
-            D 1
-            R 4
-            D 1
-            L 5
-            R 2"
-        .to_string();
+        let raw_string: String = "R 5
+        U 8
+        L 8
+        D 3
+        R 17
+        D 10
+        L 25
+        U 20"
+            .to_string();
         let v: Vec<String> = utils::test_input_to_vec(raw_string, true);
         assert_ne!(0, v.len());
         let hm = get_head_movement(&v);
         // Part 2
         {
             let mut tails: Vec<Vec<(i32, i32)>> = Vec::new();
-            let num_knots = 2;
+            let num_knots = 10;
             for i in 0..(num_knots - 1) {
                 let my_head = if i == 0 {
                     hm.clone()
@@ -184,10 +161,37 @@ mod tests {
                 let tm = get_tail_movement(&my_head, true);
                 tails.push(tm);
             }
+
+            // Now show this.
+            for time in 0..hm.len() {
+                let mut matrix: [[char; 25]; 25] = [['.'; 25]; 25];
+                // Go through each tail, choosing this timestamp
+                for _tail_idx in 0..(num_knots) {
+                    let tail_idx = num_knots - 1 - _tail_idx;
+                    // If it is 0, then use the head, otherwise use the correct tail.
+                    let points_vec_to_show = if tail_idx == 0 {
+                        hm.clone()
+                    } else {
+                        tails[tail_idx - 1].clone()
+                    };
+                    // from this tail, show this timestamp
+                    let (x, y) = points_vec_to_show[time];
+                    // Update matrix
+                    if x >= 0 && x < 25 && y >= 0 && y < 25 {
+                        let char_to_show = if tail_idx == 0 { 'H' } else {(tail_idx).to_string().chars().nth(0).unwrap()}; 
+                        matrix[(24 - y) as usize][x as usize] = char_to_show;
+                            
+                    }
+                    // For breakpoint
+                    print!("{}", 0);
+                }
+                print!("{}", 0);
+            }
+
             let mut unique_tm = tails[tails.len() - 1].clone();
             unique_tm.sort();
             unique_tm.dedup();
-            assert_eq!(13, unique_tm.len());
+            //assert_eq!(13, unique_tm.len());
             println!("Unique tail positions for part 2: {}", unique_tm.len());
         }
     }
@@ -237,6 +241,7 @@ fn main() -> io::Result<()> {
             let tm = get_tail_movement(&my_head, false);
             tails.push(tm);
         }
+
         let mut unique_tm = tails[tails.len() - 1].clone();
         unique_tm.sort();
         unique_tm.dedup();
