90562cede5126c28fb59e087ac6437e7737ad220
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-12-09 06:42:02 +0100

    Test works, but getting the wrong answer.

--- 2022/rust/day7/src/main.rs
+++ 2022/rust/day7/src/main.rs
@@ -1,94 +1,87 @@
 
-use std::{io::{self}, collections::{VecDeque, HashMap}, cmp, iter::Map};
-use itertools::Itertools;
-use std::rc::Rc;
-use std::cell::RefCell;
+use std::{io::{self}, collections::{VecDeque, HashMap}};
 mod utils;
 
 
-/*
-    We will use a key, value map (or hashmap) for each item.
-    The value will be a struct with 
-    typedef struct DirectoryNode 
-    {
-        name: String;
-        size: int; // Zero if it is a directory
-        children: Map<String, DirectoryNode>;
-    }
 
-*/
-
-type Link = Option<Box<MyHashEntry>>;
 
 // Note...
 // https://stackoverflow.com/questions/36167160/how-do-i-express-mutually-recursive-data-structures-in-safe-rust?noredirect=1&lq=1
+#[allow(dead_code)]
+fn parse_directories(v: Vec<String>)->HashMap<String, usize> {
+    let mut line_cnt = 0;
+    let mut pwd: String = String::from("");
+    let mut paths: VecDeque<String> = VecDeque::new();
+    let mut top_dir_sizes:HashMap<String, usize> = HashMap::new();
 
-#[derive(Clone)]
-struct MyHashEntry {
-    name: String,
-    size: usize,
-    parent: Link,
-    kids: HashMap<String, Box<MyHashEntry>>,
-}
 
-impl MyHashEntry {
-    pub fn new(name_in:&str) -> Self {
-        MyHashEntry { name: name_in.to_string(), size: 0, parent: None, kids: HashMap::new() }
-    }
-    pub fn set_size(&mut self, sz:usize){
-        self.size = sz;        
-    }
-    pub fn add_kid(&mut self, elem:MyHashEntry){
-        self.kids.insert(elem.name.to_string(), Box::new(MyHashEntry {
-            name:elem.name.to_string(),
-            size: elem.size,
-            parent: elem.parent,
-            kids: elem.kids,
-        }));
-    }
-    pub fn get_name(&self)->String{
-        return self.name.to_string();
-    }
-    
-
-}
-
-#[derive(Debug)]
-struct Node<T> {
-    data: T,
-    children: Vec<Node<T>>,
-}
-
-impl<T> Node<T> {
-    fn new(data: T) -> Node<T> {
-        Node { data: data, children: vec![] }
-    }
-
-    fn add_child(&mut self, child: Node<T>) {
-        self.children.push(child);
-    }
-}
-
-#[derive(Debug)]
-struct NavigableNode<'a, T: 'a> {
-    node: &'a Node<T>,
-    parent: Option<&'a NavigableNode<'a, T>>,
-}
-
-impl<'a, T> NavigableNode<'a, T> {
-    fn child(&self, index: usize) -> NavigableNode<T> {
-        NavigableNode {
-            node: &self.node.children[index],
-            parent: Some(self)
+    for next_line in v {
+        line_cnt += 1;
+        println!("line {}: {}", line_cnt, next_line);
+        let words:Vec<&str> = next_line.split_whitespace().collect();
+        if words.len() == 0 { break; }
+        if 0 == words[0].chars().count() { break; }
+        if "$" == words[0]{                
+            if "cd" == words[1]{
+                println!("\tcd {}", words[2]);
+                if ".." == words[2]{
+                    let up_path = paths.pop_back().unwrap();
+                    let up_len = up_path.len();                        
+                    let new_pwd = &pwd.to_string()[0..(pwd.len()-up_len)];
+                    pwd = new_pwd.to_string();
+                    println!("\tCurrent path is {}", pwd );
+                }                    
+                else {
+                    if words[2] == "/" {
+                        // Went to root
+                        paths.clear();
+                        pwd = String::from("");
+                    }
+                    else {
+                        // Went into a path
+                        let mut new_path: String = String::from("/");
+                        new_path.push_str(words[2].clone());                            
+                        paths.push_back(new_path.clone());
+                        pwd.push_str(&new_path.clone());
+                    }
+                    println!("\tCurrent path is {}", pwd );
+                }
+            }
+            else {
+                println!("\tTODO: command {}", words[1]);
+            }
+        } else {
+            if "dir" == words[0]{
+                println!("\tTODO: directory listing for {}", words[1]);
+            }
+            else {
+                let filesize = words[0].parse::<i32>().unwrap() as usize;
+                if 0 != filesize {
+                    println!("\tfilesize {}",filesize);
+                    // Check which top directory we are in, and push to there.
+                    if paths.len() >= 1 {
+                        for i in 0..paths.len() {
+                            let parent_dir = paths[i].clone();
+                            let curr_size_opt = top_dir_sizes.get(&parent_dir);
+                            let curr_size = if curr_size_opt == None { 0 as usize } else { *curr_size_opt.unwrap() };
+                            let entry = curr_size + filesize;
+                            top_dir_sizes.insert(parent_dir, entry);
+                        }
+                    }
+                    else {
+                        // Skip this directory
+                        println!("\tskipping file since it's at top level {}", words[1]);
+                    }
+                }
+            }
         }
     }
+
+    return top_dir_sizes;
+
 }
 
-impl<T> Node<T> {
-    fn navigate<'a>(&'a self) -> NavigableNode<T> {
-        NavigableNode { node: self, parent: None }
-    }
-}
+
 
 #[cfg(test)]
 mod tests {
@@ -102,22 +95,22 @@ mod tests {
         
         let raw_string: String = "$ cd /
         $ ls
-        dir a
+        dir apath
         14848514 b.txt
         8504156 c.dat
-        dir d
-        $ cd a
+        dir dpath
+        $ cd apath
         $ ls
-        dir e
+        dir epath
         29116 f
         2557 g
         62596 h.lst
-        $ cd e
+        $ cd epath
         $ ls
         584 i
         $ cd ..
         $ cd ..
-        $ cd d
+        $ cd dpath
         $ ls
         4060174 j
         8033020 d.log
@@ -127,60 +120,22 @@ mod tests {
         let v: Vec<String> = utils::test_input_to_vec(raw_string, true);
         assert_ne!(0, v.len());
 
-        let mut line_cnt = 0;
-        let mut pwd: String= "".to_string();
-        let mut paths: VecDeque<String> = VecDeque::new();
-        paths.push_back(pwd.to_string());
 
-        let all_files_and_paths: Vec<(&str, usize)> = Vec::new();
-
-
-        for next_line in v {
-            line_cnt += 1;
-            let words:Vec<&str> = next_line.split_whitespace().collect();
-            if words.len() == 0 { break; }
-            if 0 == words[0].chars().count() { break; }
-            if "$" == words[0]{                
-                if "cd" == words[1]{
-                    println!("line {}: cd {}", line_cnt, words[2]);
-                    if ".." == words[2]{
-                        let up_path = paths.pop_back().unwrap();
-                        let up_len = up_path.len();                        
-                        let new_pwd = &pwd.to_string()[0..(pwd.len()-up_len)];
-                        pwd = new_pwd.to_string();
-                        println!("line {}: Current path is {}", line_cnt, pwd );
-                    }                    
-                    else {
-                        if words[2] == "/" {
-                            paths.clear();
-                            pwd = String::from("/");
-                        }
-                        else {
-                            let mut new_path: String = String::from("/");
-                            new_path.push_str(words[2].clone());                            
-                            paths.push_back(new_path.clone());
-                            pwd.push_str(&new_path.clone());
-                        }
-                        println!("line {}: Current path is {}", line_cnt, pwd );
-                    }
-                }
-                else {
-                    println!("line {}: command {}", line_cnt, words[1]);
-                }
-            } else {
-                if "dir" == words[0]{
-                    println!("line {}: directory {}", line_cnt, words[1]);
-                }
-                else {
-                    let filesize = words[0].parse::<i32>().unwrap();
-                    if 0 != filesize {
-                        println!("line {}: filesize {}", line_cnt, filesize);
-                    }
-                }
-            }
-        }
+        let top_dir_sizes = parse_directories(v);
+        let mut sum_of_most_100000:usize = 0;
+        let _dirs_at_most_100000:Vec<(String, usize)> = top_dir_sizes.clone().into_iter().filter(|(_n,s)|{
+            let is_lt = *s <= 100000;
+            if is_lt {
+                sum_of_most_100000 += *s;
+            }            
+            return is_lt;
+        }).collect();
+        println!("\tSum of all directories lt 100000 {}", sum_of_most_100000);        
+        assert_eq!(95437, sum_of_most_100000);
 
     }
+
+    
 
     
 
@@ -203,8 +158,17 @@ fn main()  -> io::Result<()> {
         let l:String = line.unwrap();
         v.push(l);
     }        
-    
- 
+    let top_dir_sizes = parse_directories(v);
+    let mut sum_of_most_100000:usize = 0;
+    let _dirs_at_most_100000:Vec<(String, usize)> = top_dir_sizes.clone().into_iter().filter(|(_n,s)|{
+        let is_lt = *s <= 100000;
+        if is_lt {
+            sum_of_most_100000 += *s;
+        }            
+        return is_lt;
+    }).collect();
+    println!("\tSum of all directories lt 100000 {}", sum_of_most_100000);        
+
 
     Ok(())
 }
