bb951811f452766551a8bc6138d16c752581eb6b
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-12-10 06:37:32 +0100

    try again

--- 2022/rust/extract_secrets.sh
+++ 2022/rust/extract_secrets.sh
@@ -7,7 +7,7 @@ echo "Find all input.7z and extract them"
 set -e
 for f in `find ./ -name "input.7z"`
 do
-    DIR="$(dirname "${f}")" ; 
+    DIR="$(dirname "${f}")"
     cd ${DIR}
     7z x input.7z -p${INPUT_7Z_PASSWORD}
     cd ..
