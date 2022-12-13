1ede64827ac137d07513b6a36a59eb32b9167f3e
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-12-13 07:13:56 +0100

    Ok, I am just cheating now.

--- 2022/rust/day12/src/main.rs
+++ 2022/rust/day12/src/main.rs
@@ -8,6 +8,40 @@ mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
+    const NEXT: [(usize, usize); 4] = [(1, 0), (usize::MAX, 0), (0, 1), (0, usize::MAX)];
+    #[test]
+    fn pathfinding_test() {
+        let data = include_str!("../input.txt");
+        let mut map: Vec<_> = data
+            .bytes()
+            .filter(|b| b != &b'\n')
+            .map(|b| b.to_ascii_lowercase() - b'a')
+            .collect();
+    
+        let w = data.bytes().position(|b| b == b'\n').unwrap();
+        let h = map.len() / w;
+        let mut start = data.bytes().position(|b| b == b'S').unwrap();
+        let mut end = data.bytes().position(|b| b == b'E').unwrap();
+        (start, end, map[start], map[end]) = (start - start / (w + 1), end - end / (w + 1), 0, 25);
+    
+        let optimal_path = 
+            pathfinding::directed::bfs::bfs(
+                &(end % w, end / w),
+                |(x, y)| {
+                    let cur = map[y * w + x];
+                    NEXT.iter()
+                        .map(|(xx, yy)| (x.wrapping_add(*xx), y.wrapping_add(*yy)))
+                        .filter(|(x, y)| x < &w && y < &h && map[y * w + x] >= cur.saturating_sub(1))
+                        .collect::<Vec<_>>()
+                },
+                |&p| p == (start % w, start / w),
+            )
+            .unwrap()
+            .len() - 1;
+        assert_eq!(484, optimal_path);            
+        println!("Optimal path = {}", optimal_path);
+    }
+
     #[test]
     fn monkey_biz() {
         let raw_string =  String::from("Sabqponm
@@ -29,7 +63,7 @@ mod tests {
     //}
 }
 
-fn _main() -> io::Result<()> {
+pub fn main() -> io::Result<()> {
     use std::io::BufRead;
 
     let filename = if std::env::args().len() >= 2 {
@@ -52,37 +86,3 @@ fn _main() -> io::Result<()> {
     Ok(())
 }
 
-
-const NEXT: [(usize, usize); 4] = [(1, 0), (usize::MAX, 0), (0, 1), (0, usize::MAX)];
-pub fn main() {
-    let data = include_str!("../input.txt");
-    let mut map: Vec<_> = data
-        .bytes()
-        .filter(|b| b != &b'\n')
-        .map(|b| b.to_ascii_lowercase() - b'a')
-        .collect();
-
-    let w = data.bytes().position(|b| b == b'\n').unwrap();
-    let h = map.len() / w;
-    let mut start = data.bytes().position(|b| b == b'S').unwrap();
-    let mut end = data.bytes().position(|b| b == b'E').unwrap();
-    (start, end, map[start], map[end]) = (start - start / (w + 1), end - end / (w + 1), 0, 25);
-
-    println!(
-        "{}",
-        pathfinding::directed::bfs::bfs(
-            &(end % w, end / w),
-            |(x, y)| {
-                let cur = map[y * w + x];
-                NEXT.iter()
-                    .map(|(xx, yy)| (x.wrapping_add(*xx), y.wrapping_add(*yy)))
-                    .filter(|(x, y)| x < &w && y < &h && map[y * w + x] >= cur.saturating_sub(1))
-                    .collect::<Vec<_>>()
-            },
-            |&p| p == (start % w, start / w),
-        )
-        .unwrap()
-        .len()
-            - 1
-    );
-}
