4fda07f07639ee1a61f34550821edebd7e3e01cc
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-12-13 07:11:25 +0100

    484.

--- 2022/rust/day12/src/main.rs
+++ 2022/rust/day12/src/main.rs
@@ -1,223 +1,7 @@
 use std::{ io::{ self }, collections::VecDeque };
 mod utils;
 
-enum MonkeyOperations {
-    Squared,
-    Mul,
-    Add,
-}
 
-struct Monkey {
-    items: VecDeque<i64>,
-    divisible: i64,
-    throw_targets: (i32, i32),
-    op: MonkeyOperations,
-    op_param: i32,
-    inspections: i64,
-}
-
-impl Monkey {
-    fn new(v: &Vec<String>) -> Monkey {
-        let mut monkey = Monkey {
-            items: VecDeque::new(),
-            divisible: 0,
-            throw_targets: (-1, -1),
-            op: MonkeyOperations::Mul,
-            op_param: -1,
-            inspections: 0,
-        };
-
-        for line in v {
-            let words: Vec<&str> = line.split_whitespace().collect();
-            if "Starting" == words[0] {
-                for i in 2..words.len() {
-                    let x = utils::robust_to_int(words[i]) as i64;
-                    monkey.catch(x);
-                }
-            } else if "Operation:" == words[0] {
-                if line.contains("+") {
-                    monkey.op = MonkeyOperations::Add;
-                    monkey.op_param = utils::robust_to_int(words[5]);
-                } else if "old" == words[5] {
-                    monkey.op = MonkeyOperations::Squared;
-                    monkey.op_param = 1;
-                } else {
-                    monkey.op = MonkeyOperations::Mul;
-                    monkey.op_param = utils::robust_to_int(words[5]);
-                }
-            } else if "Test:" == words[0] {
-                let x = utils::robust_to_int(words[3]);
-                assert!(monkey.divisible == 0);
-                monkey.divisible = x as i64;
-            } else if "If" == words[0] {
-                let target_monkey = utils::robust_to_int(words[5]);
-                if "true:" == words[1] {
-                    assert!(monkey.throw_targets.0 < 0);
-                    monkey.throw_targets.0 = target_monkey;
-                } else {
-                    assert!(monkey.throw_targets.1 < 0);
-                    monkey.throw_targets.1 = target_monkey;
-                }
-            }
-        }
-        assert!(monkey.divisible > 0);
-        assert!(monkey.throw_targets.0 >= 0);
-        assert!(monkey.throw_targets.1 >= 0);
-        assert!(monkey.items.len() > 0);
-        assert!(monkey.op_param > 0);
-        return monkey;
-    }
-
-    fn catch(&mut self, item: i64) {
-        //self.items.push_back(item);
-        self.items.push_back(item);
-    }
-
-    #[allow(dead_code)]
-    fn get_items(&self) -> Vec<i64> {
-        let x: Vec<i64> = self.items
-            .iter()
-            .map(|val| {
-                let v: i64 = *val;
-                return v;
-            })
-            .collect();
-
-        return x;
-    }
-
-    fn do_operations(&mut self, modulo: i64, verbose: bool) -> Option<(i64, usize)> {
-        if self.items.len() > 0 {
-            
-            let new_item = self.items.pop_front().unwrap() as i64;
-            if verbose {
-                println!("  Monkey inspects an item with a worry level of {}.", new_item);
-            }
-            self.inspections += 1;
-
-            let mut worry_level = match self.op {
-                MonkeyOperations::Mul => {
-                    let res = new_item * (self.op_param as i64);
-                    if verbose {
-                        println!("    Worry level is multiplied by {} to {}", self.op_param, res);
-                    }
-                    res
-                }
-                MonkeyOperations::Add => {
-                    let res = new_item + (self.op_param as i64);
-                    if verbose {
-                        println!("    Worry level increases by {} to {}.", self.op_param, res);
-                    }
-                    res
-                }
-                MonkeyOperations::Squared => {
-                    let res = new_item * new_item;
-                    if verbose {
-                        println!("    Worry level is multiplied by itself to {}.", res);
-                    }
-                    res
-                }
-            };
-            if 0 == modulo {
-                worry_level = worry_level / 3;
-            } else {
-                worry_level = worry_level % modulo;
-            }
-            if verbose {
-                println!("    Monkey gets bored with item. Worry level is divided by 3 to {}.", worry_level);
-            }
-            let divisible = worry_level % self.divisible == 0;
-            if verbose {
-                if divisible {
-                    println!("    Current worry level is divisible by {}.", self.divisible);
-                } else {
-                    println!("    Current worry level is not divisible by {}.", self.divisible);
-                }
-            }
-
-            let tm = if divisible { self.throw_targets.0 } else { self.throw_targets.1 };
-            let target_monkey = tm as usize;
-
-            if verbose {
-                println!(
-                    "    Item with worry level {worry_level} is thrown to monkey {target_monkey}."
-                );
-            }
-            return Some((worry_level, target_monkey));
-        }
-
-        return None;
-    }
-}
-
-fn load_vector_into_monkeys(v: Vec<String>, monkeys: &mut Vec<Monkey>) -> i64 {
-    let mut modulo: i64 = 1;
-    let mut i = 0;
-    while i < v.len() {
-        if i >= v.len() {
-            break;
-        }
-        let line = v[i].clone();
-        i += 1;
-        let words: Vec<&str> = line.split_whitespace().collect();
-        if words.len() > 0 {
-            if "Monkey" == words[0] {
-                let new_vec: Vec<String> = v[i..i + 5].to_vec().clone();
-                i += 5;
-                let monkey = Monkey::new(&new_vec);
-                modulo *= monkey.divisible;
-                monkeys.push(monkey);
-            }
-        }
-    }
-    return modulo;
-}
-
-fn get_top_two_monkey_inspections(monkeys: Vec<Monkey>) -> Vec<i64> {
-    let mut inspections: Vec<i64> = monkeys
-        .iter()
-        .map(|m| {
-            return m.inspections;
-        })
-        .collect();
-    inspections.sort();
-    inspections.reverse();
-    inspections
-}
-
-fn monkey_business_test(v: Vec<String>, rounds: i32, really_worried: bool, verbose: bool) -> i64 {
-    let mut monkeys: Vec<Monkey> = Vec::new();
-    let modulo64 = load_vector_into_monkeys(v, &mut monkeys);
-    let modulo = if really_worried { modulo64 } else { 0 };
-
-    for _rounds in 0..rounds {
-        for m_idx in 0..monkeys.len() {
-            if verbose {
-                println!("Monkey {}:", m_idx);
-            }
-            let mut is_done: bool = false;
-            while !is_done {
-                let m = monkeys.get_mut(m_idx).unwrap();
-                let res = m.do_operations(modulo, verbose);
-                match res {
-                    None => {
-                        is_done = true;
-                    }
-                    Some(opt) => {
-                        let (wl, tm) = opt;
-                        assert!(wl >= 0);
-                        let t = monkeys.get_mut(tm).unwrap();
-                        t.catch(wl);
-                    }
-                }
-            }
-        }
-    }
-    let inspections = get_top_two_monkey_inspections(monkeys);
-    let monkey_business = inspections[0] * inspections[1];
-    println!("Monkey business = {monkey_business}");
-    return monkey_business;
-}
 
 #[cfg(test)]
 mod tests {
@@ -226,143 +10,26 @@ mod tests {
 
     #[test]
     fn monkey_biz() {
-        let raw_string: String =
-            "Monkey 0:
-        Starting items: 79, 98
-        Operation: new = old * 19
-        Test: divisible by 23
-          If true: throw to monkey 2
-          If false: throw to monkey 3
-      
-      Monkey 1:
-        Starting items: 54, 65, 75, 74
-        Operation: new = old + 6
-        Test: divisible by 19
-          If true: throw to monkey 2
-          If false: throw to monkey 0
-      
-      Monkey 2:
-        Starting items: 79, 60, 97
-        Operation: new = old * old
-        Test: divisible by 13
-          If true: throw to monkey 1
-          If false: throw to monkey 3
-      
-      Monkey 3:
-        Starting items: 74
-        Operation: new = old + 3
-        Test: divisible by 17
-          If true: throw to monkey 0
-          If false: throw to monkey 1".to_string();
+        let raw_string =  String::from("Sabqponm
+            abcryxxl
+            accszExk
+            acctuvwj
+            abdefghi");
         let v: Vec<String> = utils::test_input_to_vec(raw_string, true);
         assert_ne!(0, v.len());
 
-        let mut monkeys: Vec<Monkey> = Vec::new();
 
-        load_vector_into_monkeys(v.clone(), &mut monkeys);
-
-        assert_eq!(monkeys[0].get_items(), [79, 98].to_vec());
-        assert_eq!(monkeys[1].get_items(), [54, 65, 75, 74].to_vec());
-        assert_eq!(monkeys[2].get_items(), [79, 60, 97].to_vec());
-        assert_eq!(monkeys[3].get_items(), [74].to_vec());
-
-        for _rounds in 0..20 {
-            for m_idx in 0..monkeys.len() {
-                println!("Monkey {}:", m_idx);
-                let mut is_done: bool = false;
-                while !is_done {
-                    let m = monkeys.get_mut(m_idx).unwrap();
-                    let res = m.do_operations(0, true);
-                    match res {
-                        None => {
-                            is_done = true;
-                        }
-                        Some(opt) => {
-                            let (wl, tm) = opt;
-                            assert!(wl >= 0);
-                            let t = monkeys.get_mut(tm).unwrap();
-                            t.catch(wl);
-                        }
-                    }
-                }
-            }
-            match _rounds {
-                0 => {
-                    assert_eq!(monkeys[0].get_items(), [20, 23, 27, 26].to_vec());
-                    assert_eq!(monkeys[1].get_items(), [2080, 25, 167, 207, 401, 1046].to_vec());
-                    assert_eq!(monkeys[2].get_items(), [].to_vec());
-                    assert_eq!(monkeys[3].get_items(), [].to_vec());
-                }
-                1 => {
-                    assert_eq!(monkeys[0].get_items(), [695, 10, 71, 135, 350].to_vec());
-                    assert_eq!(monkeys[1].get_items(), [43, 49, 58, 55, 362].to_vec());
-                    assert_eq!(monkeys[2].get_items(), [].to_vec());
-                    assert_eq!(monkeys[3].get_items(), [].to_vec());
-                }
-                19 => {
-                    assert_eq!(monkeys[0].get_items(), [10, 12, 14, 26, 34].to_vec());
-                    assert_eq!(monkeys[1].get_items(), [245, 93, 53, 199, 115].to_vec());
-                    assert_eq!(monkeys[2].get_items(), [].to_vec());
-                    assert_eq!(monkeys[3].get_items(), [].to_vec());
-                    assert_eq!(monkeys[0].inspections, 101);
-                    assert_eq!(monkeys[1].inspections, 95);
-                    assert_eq!(monkeys[2].inspections, 7);
-                    assert_eq!(monkeys[3].inspections, 105);
-                }
-                _ => {}
-            }
-        }
-
-        let inspections = get_top_two_monkey_inspections(monkeys);
-
-        let monkey_business = inspections[0] * inspections[1];
-        println!("Monkey business = {monkey_business}");
-        assert_eq!(monkey_business, 10605);
+        //let monkey_business = inspections[0] * inspections[1];
+        //println!("Monkey business = {monkey_business}");
+        //assert_eq!(monkey_business, 10605);
     }
 
-    #[test]
-    fn monkey_biz_2() {
-        let raw_string: String =
-            "Monkey 0:
-        Starting items: 79, 98
-        Operation: new = old * 19
-        Test: divisible by 23
-          If true: throw to monkey 2
-          If false: throw to monkey 3
-      
-      Monkey 1:
-        Starting items: 54, 65, 75, 74
-        Operation: new = old + 6
-        Test: divisible by 19
-          If true: throw to monkey 2
-          If false: throw to monkey 0
-      
-      Monkey 2:
-        Starting items: 79, 60, 97
-        Operation: new = old * old
-        Test: divisible by 13
-          If true: throw to monkey 1
-          If false: throw to monkey 3
-      
-      Monkey 3:
-        Starting items: 74
-        Operation: new = old + 3
-        Test: divisible by 17
-          If true: throw to monkey 0
-          If false: throw to monkey 1".to_string();
-        let v: Vec<String> = utils::test_input_to_vec(raw_string, true);
-        assert_ne!(0, v.len());
-
-        if true {
-            let mb = monkey_business_test(v, 10000, true, false);
-            assert_eq!(mb, 2713310158);
-        }
-    }
+   
 
     //}
 }
 
-fn main() -> io::Result<()> {
+fn _main() -> io::Result<()> {
     use std::io::BufRead;
 
     let filename = if std::env::args().len() >= 2 {
@@ -380,16 +47,42 @@ fn main() -> io::Result<()> {
     }
     assert_ne!(0, v.len());
 
-    // Part 1
-    if true {
-        monkey_business_test(v.clone(), 20, false, true);
-    }
-
-    // Part 2
-    if true {
-        monkey_business_test(v.clone(), 10000, true, false);
-    }
 
     //assert_eq!(monkey_business, 10605);
     Ok(())
+}
+
+
+const NEXT: [(usize, usize); 4] = [(1, 0), (usize::MAX, 0), (0, 1), (0, usize::MAX)];
+pub fn main() {
+    let data = include_str!("../input.txt");
+    let mut map: Vec<_> = data
+        .bytes()
+        .filter(|b| b != &b'\n')
+        .map(|b| b.to_ascii_lowercase() - b'a')
+        .collect();
+
+    let w = data.bytes().position(|b| b == b'\n').unwrap();
+    let h = map.len() / w;
+    let mut start = data.bytes().position(|b| b == b'S').unwrap();
+    let mut end = data.bytes().position(|b| b == b'E').unwrap();
+    (start, end, map[start], map[end]) = (start - start / (w + 1), end - end / (w + 1), 0, 25);
+
+    println!(
+        "{}",
+        pathfinding::directed::bfs::bfs(
+            &(end % w, end / w),
+            |(x, y)| {
+                let cur = map[y * w + x];
+                NEXT.iter()
+                    .map(|(xx, yy)| (x.wrapping_add(*xx), y.wrapping_add(*yy)))
+                    .filter(|(x, y)| x < &w && y < &h && map[y * w + x] >= cur.saturating_sub(1))
+                    .collect::<Vec<_>>()
+            },
+            |&p| p == (start % w, start / w),
+        )
+        .unwrap()
+        .len()
+            - 1
+    );
 }
