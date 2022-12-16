7f7bef727329909b4880f37946e8d2361ec40d51
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-12-16 12:54:39 +0100

    wip

--- 2022/rust/day13/src/main.rs
+++ 2022/rust/day13/src/main.rs
@@ -10,37 +10,88 @@ mod utils;
         ];
 */
 
+enum ArrElement {
+    Integer(usize),
+    Arr(Vec<ArrElement>),
+}
 
 #[cfg(test)]
 mod tests {
+    use std::collections::VecDeque;
+
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
-    fn pathfinding_test() {
-        let data: &str = include_str!("../input.txt");
-        let data_bytes = data.bytes();
-        let data_bytes_no_newlines = data_bytes.filter(|b| b != &b'\n');
-        let mut linear_vec_u8: Vec<u8> = data_bytes_no_newlines
-            .map(|b| b.to_ascii_lowercase() - b'a')
+    fn array_compare_1() {
+        let data_bytes = String::from(
+            "[1,1,3,1,1]
+        [1,1,5,1,1]
+        
+        [[1],[2,3,4]]
+        [[1],4]
+        
+        [9]
+        [[8,7,6]]
+        
+        [[4,4],4,4]
+        [[4,4],4,4,4]
+        
+        [7,7,7,7]
+        [7,7,7]
+        
+        []
+        [3]
+        
+        [[[]]]
+        [[]]
+        
+        [1,[2,[3,[4,[5,6,7]]]],8,9]
+        [1,[2,[3,[4,[5,6,0]]]],8,9]"
+        );
+
+        let arr: Vec<ArrElement> = Vec::new();
+
+        //let data: &str = include_str!("../input.txt");
+        //let data_bytes = data.bytes();
+        let data_bytes_no_newlines: Vec<char> = data_bytes
+            .clone()
+            .chars()
+            .filter(|b| *b != '\n' && *b != ' ')
             .collect();
+        println!("len = {}", data_bytes_no_newlines.len());
+        let mut arr: Vec<ArrElement> = Vec::new();
+        let mut stack: VecDeque<Vec<ArrElement>> = VecDeque::new();
+        let mut wip_num: String = String::from("");
+        for c in data_bytes_no_newlines {
+            match c {
+                '[' => {
+                    stack.push_back(Vec::new());
+                }
+                ']' => {
+                    transfer_wip_integer(&mut wip_num, &mut stack);
+                    let vec = stack.pop_back().unwrap();
+                    arr.push(ArrElement::Arr(vec));
+                }
+                ',' => {
+                    transfer_wip_integer(&mut wip_num, &mut stack);
+                }
+                _ => {
+                    if c >= '0' && c <= '9' {
+                        wip_num.push(c);
+                    }
+                }
+            }
+        }
+    }
 
-        let w: usize = data
-            .bytes()
-            .position(|b| b == b'\n')
-            .unwrap();
-        let h: usize = linear_vec_u8.len() / w;
-
-        // Get position of S and E in the linear vector
-        let mut start_pos: usize = data
-            .bytes()
-            .position(|b| b == b'S')
-            .unwrap();
-        let mut end_pos: usize = data
-            .bytes()
-            .position(|b| b == b'E')
-            .unwrap();
-
+    fn transfer_wip_integer(wip_num: &mut String, stack: &mut VecDeque<Vec<ArrElement>>) {
+        if wip_num.len() > 0 {
+            let mut back = stack.back_mut().unwrap();
+            let x = utils::robust_to_int(wip_num) as usize;
+            back.push(ArrElement::Integer(x));
+        }
+        wip_num.clear();
     }
 
 }
