e2c4a77aaadadef535e12a9760c88085f40739a1
Chris Fogelklou <chris.fogelklou@polestar.com>
2022-12-16 15:57:44 +0100

    wip

--- 2022/rust/day13/src/main.rs
+++ 2022/rust/day13/src/main.rs
@@ -1,4 +1,4 @@
-use std::{ io::{ self } };
+use std::{io::{self}, cmp};
 mod utils;
 
 /*
@@ -15,16 +15,85 @@ enum ArrElement {
     Arr(Vec<ArrElement>),
 }
 
+fn consume_this_array(data_bytes: &Vec<char>, i: &mut usize) -> Option<ArrElement> {
+    let mut arrays: Vec<ArrElement> = Vec::new();
+    let mut done = false;
+    let mut wip_num = String::from("");
+    while !done && i < &mut data_bytes.len() {
+        let c: char = data_bytes[*i];
+        *i += 1;
+        match c {
+            '[' => {
+                let elem = consume_this_array(data_bytes, i);
+                match elem {
+                    Some(a) => match a {
+                        ArrElement::Arr(arr) => {
+                            arrays.push(ArrElement::Arr(arr));
+                        }
+                        _ => {}
+                    },
+                    _ => {}
+                }
+            }
+            ']' | ',' => {
+                done = c == ']';
+                if wip_num.len() > 0 {
+                    let x = utils::robust_to_int(&wip_num) as usize;
+                    arrays.push(ArrElement::Integer(x));
+                }
+                wip_num.clear();
+            }
+            _ => {
+                if c >= '0' && c <= '9' {
+                    wip_num.push(c);
+                }
+            }
+        }
+    }
+    return Some(ArrElement::Arr(arrays));
+}
+
+fn compare_arrays(left:&Vec<ArrElement>, right: Vec<ArrElement>){
+    let mut res:i32 = 0;
+    let mut i = 0;
+    let minlen = cmp::min(left.len(), right.len());
+    while res == 0 && i < minlen {
+        let mut left_is_vec = false;
+        let mut right_is_vec = false;
+        let mut left_int:i32 = -1;
+        let mut right_int:i32 = -1;
+        match &left[i] {
+            ArrElement::Arr(_) => {
+                left_is_vec = true;   
+            },
+            ArrElement::Integer(x) => { }
+            _ => {}
+        }
+        match &right[i] {
+            ArrElement::Arr(_) => {
+                right_is_vec = true;   
+            }
+            _ => {}
+        }        
+        if left_is_vec != right_is_vec {
+            let the_vector = if left_is_vec { &left } else { &right };
+            let not_a_vector = if !left_is_vec { right } else { left };
+        }
+        i += 1;
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::collections::VecDeque;
+
+    use std::array;
 
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
     fn array_compare_1() {
-        let data_bytes = String::from(
+        let mut data_bytes = String::from(
             "[1,1,3,1,1]
         [1,1,5,1,1]
         
@@ -47,53 +116,44 @@ mod tests {
         [[]]
         
         [1,[2,[3,[4,[5,6,7]]]],8,9]
-        [1,[2,[3,[4,[5,6,0]]]],8,9]"
+        [1,[2,[3,[4,[5,6,0]]]],8,9]",
         );
 
-        let arr: Vec<ArrElement> = Vec::new();
-
-        //let data: &str = include_str!("../input.txt");
-        //let data_bytes = data.bytes();
         let data_bytes_no_newlines: Vec<char> = data_bytes
             .clone()
             .chars()
             .filter(|b| *b != '\n' && *b != ' ')
             .collect();
-        println!("len = {}", data_bytes_no_newlines.len());
-        let mut arr: Vec<ArrElement> = Vec::new();
-        let mut stack: VecDeque<Vec<ArrElement>> = VecDeque::new();
-        let mut wip_num: String = String::from("");
-        for c in data_bytes_no_newlines {
-            match c {
-                '[' => {
-                    stack.push_back(Vec::new());
-                }
-                ']' => {
-                    transfer_wip_integer(&mut wip_num, &mut stack);
-                    let vec = stack.pop_back().unwrap();
-                    arr.push(ArrElement::Arr(vec));
-                }
-                ',' => {
-                    transfer_wip_integer(&mut wip_num, &mut stack);
-                }
-                _ => {
-                    if c >= '0' && c <= '9' {
-                        wip_num.push(c);
-                    }
+
+        let mut arrays: Vec<ArrElement> = Vec::new();
+
+        let mut i = 0;
+        while i < data_bytes_no_newlines.len() {
+            let c: char = data_bytes_no_newlines[i];
+            i += 1;
+            if c == '[' {
+                let elem = consume_this_array(&data_bytes_no_newlines, &mut i);
+                match elem {
+                    Some(a) => match a {
+                        ArrElement::Arr(arr) => {
+                            arrays.push(ArrElement::Arr(arr));
+                        }
+                        _ => {}
+                    },
+                    _ => {}
                 }
             }
         }
-    }
 
-    fn transfer_wip_integer(wip_num: &mut String, stack: &mut VecDeque<Vec<ArrElement>>) {
-        if wip_num.len() > 0 {
-            let mut back = stack.back_mut().unwrap();
-            let x = utils::robust_to_int(wip_num) as usize;
-            back.push(ArrElement::Integer(x));
+        for i in (0..arrays.len()).step_by(2) {
+            let res = compare_arrays(&arrays[i+0], &arrays[i+1]);
+            if (0 == i){
+                assert!(res > 1);
+            }
         }
-        wip_num.clear();
-    }
 
+        println!("len = {}", data_bytes_no_newlines.len());
+    }
 }
 
 pub fn main() -> io::Result<()> {
@@ -105,7 +165,7 @@ pub fn main() -> io::Result<()> {
         String::from("input.txt")
     };
     let file = std::io::BufReader::new(
-        std::fs::File::open(<String as AsRef<std::path::Path>>::as_ref(&filename)).unwrap()
+        std::fs::File::open(<String as AsRef<std::path::Path>>::as_ref(&filename)).unwrap(),
     );
     let mut vec_in: Vec<String> = Vec::new();
     for (_, line) in file.lines().enumerate() {
