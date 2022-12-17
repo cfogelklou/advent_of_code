e5aa2ae37bcd24986877a1805e6c3abc533afc71
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-12-17 08:43:22 +0100

    wip

--- 2022/rust/day14/src/main.rs
+++ 2022/rust/day14/src/main.rs
@@ -13,8 +13,6 @@ use std::collections::HashMap;
         ];
 */
 
-
-
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -23,42 +21,68 @@ mod tests {
     #[test]
     fn array_compare_1() {
         let data_bytes = String::from(
-            "498,4 -> 498,6 -> 496,6
+            "5,0 -> 0,0
+            498,4 -> 498,6 -> 496,6
             503,4 -> 502,4 -> 502,9 -> 494,9"
         );
         let vec_in: Vec<String> = utils::test_input_to_vec(data_bytes, true);
         assert_ne!(0, vec_in.len());
 
-        let mut world:HashMap<String, char> = HashMap::new();
+        let mut world: HashMap<(i32,i32), char> = HashMap::new();
 
         for line in vec_in {
-            let sxy:Vec<(&str, i32,i32)> = line.split("->").map(|l| l.trim() ).map(|l2| {
-                let v:Vec<&str> = l2.split(",").collect();                
-                let x = utils::robust_to_int(v[0]);
-                let y = utils::robust_to_int(v[1]);
-                return (l2, x,y);
-            }).collect();
+            let xy: Vec<(i32, i32)> = line
+                .split("->")
+                .map(|l| l.trim())
+                .map(|l2| {
+                    let v: Vec<&str> = l2.split(",").collect();
+                    let x = utils::robust_to_int(v[0]);
+                    let y = utils::robust_to_int(v[1]);
+                    return (x, y);
+                })
+                .collect();
 
-            println!("len = {}", sxy.len());
+            println!("len = {}", xy.len());
 
-            for item in sxy {
-                world.insert(item.0.to_string(), '#');
+            for i in 0..xy.len()-1 {
+                let i0 = xy[i+0];
+                let i1 = xy[i+1];
+                if i0.1 != i1.1 {
+                    assert_eq!(i0.0, i1.0);
+                    let y0 = if i0.1 < i1.1 {i0.1} else {i1.1};
+                    let y1 = if i0.1 < i1.1 {i1.1} else {i0.1};
+                    for y in y0..y1+1 {                    
+                        world.insert((i0.0, y), '#');
+                    }
+                }
+                else if i0.0 != i1.0 {
+                    assert_eq!(i0.1, i1.1);
+                    let x0 = if i0.0 < i1.0 {i0.0} else {i1.0};
+                    let x1 = if i0.0 < i1.0 {i1.0} else {i0.0};
+                    for x in x0..x1+1 {                    
+                        world.insert((x, i0.1), '#');
+                    }    
+                }
+                else {
+                    assert!(false);
+                }
+
             }
-            
-            
+
+            //for item in sxy {
+            //    world.insert(item.0.to_string(), '#');
+            //}
         }
     }
 }
 
 pub fn main() -> io::Result<()> {
-
     let filename = if std::env::args().len() >= 2 {
         std::env::args().nth(1).unwrap()
     } else {
         String::from("input.txt")
     };
     let data_bytes = std::fs::read_to_string(filename).unwrap();
-
 
     Ok(())
 }
